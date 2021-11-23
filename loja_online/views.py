5f235f2a8fdc869819181ac375c322d463c4a05b
Vinicius <vinicius.b.silva3@gmail.com>
2021-11-23 14:51:56 -0300

    dados_comuns adicionado ao context do template 404.html, e cor do header Produtos semelhantes da página do produto atualizada

--- loja_online/views.py
+++ loja_online/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from dados_comuns import dados_comuns
 
 
 def page_error_404_view(request, exception):
-    return render(request, '404.html', status=404)
+    return render(request, '404.html', status=404, context=dados_comuns(request.user))
